0efe5fe98fafde5e02655395f504eb93a7f4760b
Srinivas Bavirisetti (accenture) <srinivas.bavirisetti@accenture.com>
2016-12-22 11:57:06 +0530

    POC source code

---
+++ IFS_AutoSample/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace IFS_AutoSample
+{
+    [Activity(Label = "IFS_AutoSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            EditText userName = FindViewById<EditText>(Resource.Id.userName);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            TextView lblResult = FindViewById<TextView>(Resource.Id.lblResult);
+
+            button.Click += delegate
+            {
+                if (userName.Text == "ganesh" && password.Text == "ganesh")
+                {
+                    lblResult.Text = "Login Successfull";
+                }
+                else
+                {
+                    lblResult.Text = "Login Failed";
+                }
+            };
+        }
+    }
+}
+
