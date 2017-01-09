68421a4594f8c16717e8288d5b3f25a2929a8d43
Srinivas Bavirisetti (accenture) <srinivas.bavirisetti@accenture.com>
2017-01-09 17:50:39 +0530

    Updated with new solution

---
+++ IFS_AutoSample/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace IFS_AutoSample
+{
+    [Activity(Label = "IFS_AutoSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            EditText userName = FindViewById<EditText>(Resource.Id.userName);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            
+
+            button.Click += delegate
+            {
+                if (userName.Text == "ganesh" && password.Text == "ganesh")
+                {
+					StartActivity(typeof(HomeActivity));
+					//var msgDiaglog = new AlertDialog.Builder(this);
+					//msgDiaglog.SetMessage("Login Successfull");
+					//msgDiaglog.SetPositiveButton("OK", (senderAlert, args) =>
+					//{
+					//	// write your own set of instructions
+					//});
+					//msgDiaglog.Show();
+
+                    //lblResult.Text = "Login Successfull";
+                }
+                else
+                {
+					var msgDiaglog = new AlertDialog.Builder(this);
+					msgDiaglog.SetMessage("Login Failed");
+					msgDiaglog.SetPositiveButton("OK", (senderAlert, args) =>
+					{
+						// write your own set of instructions
+					});
+					msgDiaglog.Show();
+                    //lblResult.Text = "Login Failed";
+                }
+            };
+        }
+    }
+}
+
